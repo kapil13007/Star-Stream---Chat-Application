233a545ff08f114a30b63ef330578197ed9a9604
kapil13007 <kapil.loginhdi@gmail.com>
2026-03-09 17:09:25 +0530

    Added logout and login API's

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -58,3 +58,34 @@ export const signup = async(req,res) =>{
         res.status(500).json({message:"Internal Server error"});
     }
 };
+
+
+export const login = async(req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+        // compare entered password with hashed password in DB
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials" });
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch(error) {
+        console.log("error in login controller:", error);
+        res.status(500).json({ message: "Internal Server error" });
+    }
+};
+
+export const logout = (req,res) => {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:"Logged out successfully"});
+};
