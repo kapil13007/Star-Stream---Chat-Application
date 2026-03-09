dd72aff441e0d062a874a8b872d4341ee4e80e6b
kapil13007 <kapil.loginhdi@gmail.com>
2026-03-09 14:12:59 +0530

    Added welcome email functionality

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
+import { ENV } from './env.js';
 
 export const generateToken = (userId,res) =>{
     
-    const{JWT_SECRET} = process.env;
+    const{JWT_SECRET} = ENV;
     if(!JWT_SECRET){
         throw new Error("JWT Secret is not configured");
     }
@@ -14,7 +15,7 @@ export const generateToken = (userId,res) =>{
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly:true,//prevent XSS attacks
         sameSite:"strict", // CSRF Attacks
-        secure:process.env.NODE_ENV === "development" ? false : true,
+        secure:ENV.NODE_ENV === "development" ? false : true,
     });
     return token;
 };
