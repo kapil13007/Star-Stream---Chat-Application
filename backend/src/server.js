af3ac90e292addd3c7643b0a9f5a6dc2f6f2e1fc
kapil13007 <kapil.loginhdi@gmail.com>
2026-03-09 18:46:54 +0530

    added change profile pic feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from "./routes/auth.route.js";
 import path from 'path';
+import cookieParser from 'cookie-parser';
 import {connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 
@@ -9,7 +10,9 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+//app.use("/api/messages",messageRoutes);
 
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
